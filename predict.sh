aef8e9b49a1ece14c622a7c4467be75029e6e5a5
NURx2 <inurx2@gmail.com>
2021-05-29 00:33:32 +0300

    remove excess code from predict.sh

--- predict.sh
+++ predict.sh
@@ -23,9 +23,9 @@ rm ${TARGET_HISTOGRAM_FILE} ${ORIGIN_HISTOGRAM_FILE} ${PATH_HISTOGRAM_FILE}
 
 echo "### Preprocessing is done ###"
 
-${PYTHON} -u code2vec/code2vec.py --framework tensorflow --load ${MODEL_DIR}/saved_model_iter8 --test ${TEST_DATA} --export_code_vectors
+${PYTHON} -u code2vec/code2vec.py --framework tensorflow --load ${MODEL_DIR}/saved_model --test ${TEST_DATA} --export_code_vectors
 
-${PYTHON} -u code2vec/code2vec.py --framework tensorflow --load ${MODEL_DIR}/saved_model_iter8 --save_t2v ${TARGET_VECTORS_FILE_PATH}
+${PYTHON} -u code2vec/code2vec.py --framework tensorflow --load ${MODEL_DIR}/saved_model --save_t2v ${TARGET_VECTORS_FILE_PATH}
 
 rm -f log.txt keras_model_log.txt
 
